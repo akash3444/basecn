4de5e80ac4dc63f871943e5f05e689227ccd1135
akash3444 <akashmoradiya3444@gmail.com>
2025-08-13 10:00:38 +0530

    feat: add hero section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,27 @@
-import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+    <main className="flex flex-1 flex-col text-center py-20">
+      <div className="max-w-3xl mx-auto">
+        <h1 className="mb-4 text-4xl sm:text-5xl md:text-7xl font-medium tracking-[-0.06em] text-balance leading-snug">
+          shadcn/ui Components Powered by{" "}
+          <span className="bg-primary px-4 py-1 rounded text-primary-foreground leading-none inline-block">
+            Base UI
+          </span>
+        </h1>
+        <p className="text-fd-muted-foreground text-xl">
+          Beautifully crafted shadcn/ui components powered by Base UI, providing
+          a solid foundation to build modern and elegant interfaces with speed.
+        </p>
+        <Button className="mt-8" size="xl" asChild>
+          <Link href="/docs/components/avatar">
+            Get Started <ArrowUpRight />
+          </Link>
+        </Button>
+      </div>
     </main>
   );
 }
