70d32525c93fc389c699825a46e3dc3d93b80cea
akash3444 <akashmoradiya3444@gmail.com>
2025-08-14 22:31:22 +0530

    feat: add logo

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { Bolt } from "lucide-react";
 
 /**
  * Shared layout configurations
@@ -10,17 +11,10 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
-      <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        basecn
-      </>
+      <div className="flex items-center gap-2">
+        <Bolt className="text-foreground/70 fill-foreground/10" />
+        <span className="text-lg font-bold">basecn</span>
+      </div>
     ),
   },
   // see https://fumadocs.dev/docs/ui/navigation/links
