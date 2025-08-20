13254eb7aa12f2d3d5a75c46a78a247b1e9be0db
akash3444 <akashmoradiya3444@gmail.com>
2025-08-20 21:33:56 +0530

    feat: add trackWebVitals property to analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,7 @@ export default function Layout({ children }: { children: ReactNode }) {
             trackInteractions
             trackEngagement
             trackBounceRate
+            trackWebVitals
             enableBatching
           />
         </RootProvider>
