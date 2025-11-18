8ec5e155c7139791dbe52e92a38f1cd27e3478aa
akash3444 <akashmoradiya3444@gmail.com>
2025-11-18 20:56:56 +0530

    chore: integrate wandry analytics

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { captureRegistryEvent } from "@wandry/analytics-sdk";
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  captureRegistryEvent(
+    request,
+    "31|3SGJFvi5TFqN6yukbwLJbpCOS9P31cUd0JdddrNh0a077f4c"
+  );
+  return NextResponse.next();
+}
